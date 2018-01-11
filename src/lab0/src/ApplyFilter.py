25a8580366b581364e9a9a3eb3383623b6c78ec2
matthewngo <mvn3@uw.edu>
2018-01-10 18:25:12 -0800

    cleaned up files

--- src/lab0/src/ApplyFilter.py
+++ src/lab0/src/ApplyFilter.py
@@ -2,7 +2,6 @@
 
 import rospy
 import numpy as np
-from scipy import signal
 from  scipy import ndimage
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -10,58 +9,59 @@ from cv_bridge import CvBridge, CvBridgeError
 class Filter:
 	def __init__(self, filter_path, sub_topic, pub_topic, fast_convolve=False):
 		# Load the filter from csv
-		filter_array = np.loadtxt(filter_path, delimiter=',', ndmin=2)
+		filter_from_csv = np.loadtxt(filter_path, delimiter=',', ndmin=2)
+
 		# Create the publisher and subscriber
-		publisher = rospy.Publisher(pub_topic, Image, queue_size=10)
-		subscriber = rospy.Subscriber(sub_topic, Image, self.apply_filter_cb, queue_size=10)
+		pub = rospy.Publisher(pub_topic, Image, queue_size=10)
+		sub = rospy.Subscriber(sub_topic, Image, self.apply_filter_cb, queue_size=10)
+
 		# Create a CvBridge object for converting sensor_msgs/Image into numpy arrays (and vice-versa)
 		#		http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
-		bridge = CvBridge()
 		# Use the 'self' parameter to initialize as necessary
-		self.converter = bridge
-		self.publisher = publisher
-		self.filter_array = filter_array
+                self.filter_array = filter_from_csv
+                self.pub = pub
+		self.bridge = CvBridge()
 		self.fast_convolve = fast_convolve
 
 	def apply_filter_cb(self, msg):
 		# Apply the filter to the incoming image and publish the result
-		cv_img = self.converter.imgmsg_to_cv2(msg)
-		result_cv_image = np.zeros([cv_img.shape[0], cv_img.shape[1], cv_img.shape[2]])
-		if self.fast_convolve:
-			result_cv_image[:,:,2] = ndimage.filters.convolve(cv_img[:,:,0], self.filter_array)
-			result_cv_image[:,:,1] = ndimage.filters.convolve(cv_img[:,:,1], self.filter_array)
-			result_cv_image[:,:,0] = ndimage.filters.convolve(cv_img[:,:,2], self.filter_array)
+		cv = self.converter.imgmsg_to_cv2(msg)
+		result_cv = np.zeros([cv.shape[0], cv.shape[1], cv.shape[2]])
+                if self.fast_convolve:
+			result_cv[:,:,2] = ndimage.filters.convolve(cv[:,:,0], self.filter_array)
+			result_cv[:,:,1] = ndimage.filters.convolve(cv[:,:,1], self.filter_array)
+			result_cv[:,:,0] = ndimage.filters.convolve(cv[:,:,2], self.filter_array)
 		else:
-			for i in range(0, cv_img.shape[0] - self.filter_array.shape[0]):
-				for j in range(0, cv_img.shape[1] - self.filter_array.shape[1]):
+			for i in range(0, cv.shape[0] - self.filter_array.shape[0]):
+				for j in range(0, cv.shape[1] - self.filter_array.shape[1]):
 					total = [0, 0, 0]
 					for x in range(0, self.filter_array.shape[0]):
 						for y in range(0, self.filter_array.shape[1]):
-							total[0] += cv_img[i+x, j+y, 0] * self.filter_array[x, y]
-							total[1] += cv_img[i+x, j+y, 1] * self.filter_array[x, y]
-							total[2] += cv_img[i+x, j+y, 2] * self.filter_array[x, y]
-					result_cv_image[i+1, j+1, 0] = total[0]
-					result_cv_image[i+1, j+1, 1] = total[1]
-					result_cv_image[i+1, j+1, 2] = total[2]
+							total[0] += cv[i+x, j+y, 0] * self.filter_array[x, y]
+							total[1] += cv[i+x, j+y, 1] * self.filter_array[x, y]
+							total[2] += cv[i+x, j+y, 2] * self.filter_array[x, y]
+					result_cv[i+1, j+1, 0] = total[0]
+					result_cv[i+1, j+1, 1] = total[1]
+					result_cv[i+1, j+1, 2] = total[2]
 					
-					
-		result_cv_image = result_cv_image.astype(np.uint8)
-		result_msg = self.converter.cv2_to_imgmsg(result_cv_image)
+		result_cv = result_cv.astype(np.uint8)
+		result_msg = self.bridge.cv2_to_imgmsg(result_cv)
+
 		# If the image has multiple channels, apply the filter to each channel independent of the other channels
-		self.publisher.publish(result_msg)
+		self.pub.publish(result_msg)
 		
 if __name__ == '__main__':
-	filter_path = rospy.get_param('filter_path') #The path to the csv file containing the filter
-	sub_topic = rospy.get_param('sub_topic') # The image topic to be subscribed to
-	pub_topic = rospy.get_param('pub_topic') # The topic to publish filtered images to
-	fast_convolve = rospy.get_param('fast_convolve') # Whether or not the nested for loop or Scipy's convolve method should be used for applying the filter
+	filter_path = rospy.get_param("filter_path") #The path to the csv file containing the filter
+	sub_topic = rospy.get_param("subscriber") # The image topic to be subscribed to
+	pub_topic = rospy.get_param("publisher") # The topic to publish filtered images to
+	fast_convolve = rospy.get_param("fast_convolve') # Whether or not the nested for loop or Scipy's convolve method should be used for applying the filter
 
 	rospy.init_node('apply_filter', anonymous=True)
 	
 	# Populate params with values passed by launch file
 
 	# Create a Filter object and pass it the loaded parameters
-	filter1 = Filter(filter_path, sub_topic, pub_topic, fast_convolve)
+	f = Filter(filter_path, sub_topic, pub_topic, fast_convolve)
 
 	rospy.spin()
 	
--- src/lab0/src/BagFollower.py
+++ src/lab0/src/BagFollower.py
@@ -4,12 +4,11 @@ import rospy
 import rosbag
 from ackermann_msgs.msg import AckermannDriveStamped
 
-BAG_TOPIC = 'vesc/low_level/ackermann_cmd_mux/input/teleop'# Name of the topic that should be extracted from the bag
+BAG_TOPIC = 'vesc/low_level/ackermann_cmd_mux/input/teleop' # Name of the topic that should be extracted from the bag
 PUB_TOPIC = '/vesc/high_level/ackermann_cmd_mux/input/nav_0'
-PUB_RATE = 20# The rate at which messages should be published
+PUB_RATE = 20 # The rate at which messages should be published
 
 def follow_bag(bag_path, follow_backwards=False):
-	#pass
         bag = rosbag.Bag(bag_path)
         pub = rospy.Publisher(PUB_TOPIC, AckermannDriveStamped, queue_size=10)
         r = rospy.Rate(PUB_RATE)
