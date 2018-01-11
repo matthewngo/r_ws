6bc9d388cc46bfbbbb6bc4143a609416f6d4348d
mvn3 <mvn3@roland.csenetid.cs.washington.edu>
2018-01-10 16:03:53 -0800

    finished lab0

--- src/lab0/src/ApplyFilter.py
+++ src/lab0/src/ApplyFilter.py
@@ -21,14 +21,30 @@ class Filter:
 		self.converter = bridge
 		self.publisher = publisher
 		self.filter_array = filter_array
+		self.fast_convolve = fast_convolve
 
 	def apply_filter_cb(self, msg):
 		# Apply the filter to the incoming image and publish the result
 		cv_img = self.converter.imgmsg_to_cv2(msg)
 		result_cv_image = np.zeros([cv_img.shape[0], cv_img.shape[1], cv_img.shape[2]])
-		result_cv_image[:][:][0] = ndimage.filters.convolve(cv_img[:][:][0], self.filter_array)
-		result_cv_image[:][:][1] = ndimage.filters.convolve(cv_img[:][:][1], self.filter_array)
-		result_cv_image[:][:][2] = ndimage.filters.convolve(cv_img[:][:][2], self.filter_array)
+		if self.fast_convolve:
+			result_cv_image[:,:,2] = ndimage.filters.convolve(cv_img[:,:,0], self.filter_array)
+			result_cv_image[:,:,1] = ndimage.filters.convolve(cv_img[:,:,1], self.filter_array)
+			result_cv_image[:,:,0] = ndimage.filters.convolve(cv_img[:,:,2], self.filter_array)
+		else:
+			for i in range(0, cv_img.shape[0] - self.filter_array.shape[0]):
+				for j in range(0, cv_img.shape[1] - self.filter_array.shape[1]):
+					total = [0, 0, 0]
+					for x in range(0, self.filter_array.shape[0]):
+						for y in range(0, self.filter_array.shape[1]):
+							total[0] += cv_img[i+x, j+y, 0] * self.filter_array[x, y]
+							total[1] += cv_img[i+x, j+y, 1] * self.filter_array[x, y]
+							total[2] += cv_img[i+x, j+y, 2] * self.filter_array[x, y]
+					result_cv_image[i+1, j+1, 0] = total[0]
+					result_cv_image[i+1, j+1, 1] = total[1]
+					result_cv_image[i+1, j+1, 2] = total[2]
+					
+					
 		result_cv_image = result_cv_image.astype(np.uint8)
 		result_msg = self.converter.cv2_to_imgmsg(result_cv_image)
 		# If the image has multiple channels, apply the filter to each channel independent of the other channels
@@ -45,7 +61,7 @@ if __name__ == '__main__':
 	# Populate params with values passed by launch file
 
 	# Create a Filter object and pass it the loaded parameters
-	filter = Filter(filter_path, sub_topic, pub_topic, fast_convolve)
+	filter1 = Filter(filter_path, sub_topic, pub_topic, fast_convolve)
 
 	rospy.spin()
 	
