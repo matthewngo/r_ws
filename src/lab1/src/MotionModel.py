b1e86b70e4f95891aa6bf55084a57ef169c12341
novinc <novinc@roland.csenetid.cs.washington.edu>
2018-01-22 19:27:35 -0800

    Kinematic model

--- src/lab1/src/MotionModel.py
+++ src/lab1/src/MotionModel.py
@@ -30,9 +30,9 @@ class OdometryMotionModel:
       # YOUR CODE HERE
       # deterministic, get the control (delta_x, delta_y, delta_theta) from msg - last_pose
       # assuming rotation is around the y-axis and we're in a flat 2D world
-      control = (curr_pose[0] - self.last_pose[0],
+      control = [curr_pose[0] - self.last_pose[0],
                  curr_pose[1] - self.last_pose[1],
-                 curr_pose[2] - self.last_pose[2])
+                 curr_pose[2] - self.last_pose[2]]
     
       self.apply_motion_model(self.particles, control)
 
@@ -81,6 +81,9 @@ class KinematicMotionModel:
     # Convert raw msgs to controls
     # Note that control = (raw_msg_val - offset_param) / gain_param
     # YOUR CODE HERE
+    curr_speed = (msg.state.speed - self.SPEED_TO_ERPM_OFFSET) / self.SPEED_TO_ERPM_GAIN
+    curr_steering_angle = (self.last_servo_cmd - self.STEERING_TO_SERVO_OFFSET) / self.STEERING_TO_SERVO_GAIN
+    dt = msg.header.stamp - self.last_vesc_stamp
     
     self.apply_motion_model(proposal_dist=self.particles, control=[curr_speed, curr_steering_angle, dt])
     self.last_vesc_stamp = msg.header.stamp
