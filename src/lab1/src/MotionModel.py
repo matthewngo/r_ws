fixed sensor model normalizing

src/lab1/src/MotionModel.py
@@ -33,10 +33,6 @@ class OdometryMotionModel:
       control = [curr_pose[0] - self.last_pose[0],
                  curr_pose[1] - self.last_pose[1],
                  curr_pose[2] - self.last_pose[2]]
-      # adding noise
-      control[0] += np.random.normal(0, 1)
-      control[1] += np.random.normal(0, 1)
-      control[2] += np.random.normal(0, 1)
 
     
       self.apply_motion_model(self.particles, control)
@@ -48,9 +44,9 @@ class OdometryMotionModel:
     # Update the proposal distribution by applying the control to each particle
     # YOUR CODE HERE
     for row in proposal_dist:
-      row[0] += control[0]
-      row[1] += control[1]
-      row[2] += control[2]
+      row[0] += control[0] + np.random.normal(0, 0.1)
+      row[1] += control[1] + np.random.normal(0, 0.1)
+      row[2] += control[2] + np.random.normal(0, 0.1)
     
 class KinematicMotionModel:
 
@@ -92,9 +88,6 @@ class KinematicMotionModel:
     curr_speed = (msg.state.speed - self.SPEED_TO_ERPM_OFFSET) / self.SPEED_TO_ERPM_GAIN
     curr_steering_angle = (self.last_servo_cmd - self.STEERING_TO_SERVO_OFFSET) / self.STEERING_TO_SERVO_GAIN
     dt = msg.header.stamp - self.last_vesc_stamp
-
-    curr_speed += np.random.normal(0, 1)
-    curr_steering_angle += np.random.normal(0, 1)
     
     self.apply_motion_model(proposal_dist=self.particles, control=[curr_speed, curr_steering_angle, dt])
     self.last_vesc_stamp = msg.header.stamp
@@ -104,10 +97,10 @@ class KinematicMotionModel:
     # Update the proposal distribution by applying the control to each particle
     
     for row in proposal_dist:
-      delta_x = control[0] * np.cos(row[2])
-      delta_y = control[0] * np.sin(row[2])
-      beta = np.arctan(0.5 * np.tan(control[1]))
-      delta_theta = (control[0] / 0.33) * np.sin(2 * beta)
+      delta_x = (control[0]+np.random.normal(0, 0.1)) * np.cos(row[2])
+      delta_y = (control[0]+np.random.normal(0, 0.1)) * np.sin(row[2])
+      beta = np.arctan(0.5 * np.tan(control[1]+np.random.normal(0, 0.1)))
+      delta_theta = ((control[0]+np.random.normal(0, 0.1)) / 0.33) * np.sin(2 * beta)
       row[0] += delta_x
       row[1] += delta_y
       row[2] += delta_theta

src/lab1/src/SensorModel.py
@@ -81,14 +81,14 @@ class SensorModel:
     probs_normal = np.zeros([table_width, table_width])
     probs_expon = np.zeros([table_width, table_width])
     probs_max = np.zeros([table_width, table_width])
-    probs_uniform = np.zeros([table_width, table_width])
+    probs_uniform = np.zeros([table_width, table_width], dtype=np.float64)
     for row in range(sensor_model_table.shape[0]):
       for column in range (sensor_model_table.shape[1]):
         probs_normal[row, column] = stats.norm.pdf(row, column, 1)
         probs_expon[row, column] = stats.expon.pdf(row, 0, 1)
-        if column == max_range_px:
+        if row == max_range_px:
           probs_max[row, column] = 1
-        probs_uniform[row, column] = 1 / max_range_px
+        probs_uniform[row, column] = np.float64(1.0) / max_range_px
 
     probs_normal = probs_normal / probs_normal.sum(axis=0, keepdims=1)
     probs_expon = probs_expon / probs_expon.sum(axis=0, keepdims=1)
@@ -99,7 +99,11 @@ class SensorModel:
       for column in range (sensor_model_table.shape[1]):
         sensor_model_table[row, column] = (probs_normal[row, column] * weights[0]) + (probs_expon[row, column] * weights[1]) + (probs_max[row, column] * weights[2]) + (probs_uniform[row, column] * weights[3])
 
-    sensor_model_table.tofile("/home/mvn3/table.txt", sep=",")     
+    print 'normal sums', probs_normal.sum(axis=0)
+    print 'expon sums', probs_expon.sum(axis=0)
+    print 'max sums', probs_max.sum(axis=0)
+    print 'uniform sums', probs_uniform.sum(axis=0)
+    print max_range_px
     return sensor_model_table
 
   def apply_sensor_model(self, proposal_dist, obs, weights):