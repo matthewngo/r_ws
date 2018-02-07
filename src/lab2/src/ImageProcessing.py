11cc9848a92664befd50f8f26d06f262281e4a7f
mlrobins <mlrobins@sparks.csenetid.cs.washington.edu>
2018-02-06 16:19:58 -0800

    more lab2 stub files

---
+++ src/lab2/src/ImageProcessing.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy 
+import numpy as np
+from threading import Lock
+
+
+
+class ImageProcessor:
+
+	def __init__(self, state_lock=None):
+		#initialize stuff
+		if state_lock is None:
+			self.state_lock = Lock()
+		else:
+			self.state_lock = state_lock
+
+	def image_cb(self, msg):
+		self.state_lock.acquire()
+
+		#do image processing
+
+		self.state_lock.release()
+
+	def get_error(self):
+		#return the current error for the processed image
---
+++ src/lab2/src/LineFollower.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import rospy 
+import numpy as np
+from threading import Lock
+
+from ImageProcessing import ImageProcessor
+
+
+class LineFollower:
+
+	def __init__(self):
+		#initialize stuff
+		self.state_lock = Lock()
+
+		#DEFFO tweak these later
+		self.k_p = 3
+		self.k_i = 3
+		self.k_d = 3
+
+		#hook your image processor up to the topic
+		self.image_processor = ImageProcessor(self.state_lock)
+		self.image_sub = rospy.Subscriber(rospy.get_param("~image_topic", "TODO: image topic name"), TODOIMAGETYPE, self.image_processor.image_cb, queue_size=1)
+
+	def angle(self):
+		#return the angle you go to
+	
+if __name__ == '__main__':
+	rospy.init_node("line_follower", anonymous=True)
+	lf = LineFollower()
+
+	while not rospy.is_shutdown():
+		#drive forward with constant speed and lf.angle
