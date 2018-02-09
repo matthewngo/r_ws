90b02098a3795fac2030c5a1e979d05723391432
mlrobins <mlrobins@bane.csenetid.cs.washington.edu>
2018-02-08 16:27:51 -0800

    working on making the car drive

--- src/lab2/src/LineFollower.py
+++ src/lab2/src/LineFollower.py
@@ -5,6 +5,7 @@ import numpy as np
 from threading import Lock
 
 from sensor_msgs.msg import Image
+from ackermann_msgs.msg import AckermannDriveStamped
 
 from ImageProcessing import ImageProcessor
 
@@ -19,14 +20,19 @@ class LineFollower:
 		self.k_p = 3
 		self.k_i = 3
 		self.k_d = 3
+		self.speed = 0.25
 
 		#hook your image processor up to the topic
 		self.image_processor = ImageProcessor(self.state_lock)
 		self.image_sub = rospy.Subscriber(rospy.get_param("~image_topic", "/camera/color/image_raw"), Image, self.image_processor.image_cb, queue_size=1)
 
+		self.pub_drive = rospy.Publisher(rospy.get_param("~drive_topic", "/vesc/high_level/ackermann_cmd_mux/input/nav_0"), AckermannDriveStamped, queue_size = 1)
+
 	def angle(self):
-		#return the angle you go to
-		pass
+		self.state_lock.acquire()
+		ret = self.k_p*self.image_processor.curr_error + self.k_i*self.image_processor.total_error + self.k_d*self.image_processor.delta_error
+		self.state_lock.release()
+		return ret
 
 if __name__ == '__main__':
 	rospy.init_node("line_follower", anonymous=True)
@@ -34,4 +40,10 @@ if __name__ == '__main__':
 
 	while not rospy.is_shutdown():
 		#drive forward with constant speed and lf.angle
-		i = 3
+		msg = AckermannDriveStamped()
+		msg.header.stamp = rospy.Time.now()
+		msg.header.frame_id = "base_link"
+		msg.drive.steering_angle = lf.angle()
+		msg.drive.speed = lf.speed
+		print lf.angle()
+		lf.pub_drive.publish(msg)
