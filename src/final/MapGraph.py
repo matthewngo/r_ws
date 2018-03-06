49f774d327f2d20c123b513c59b1e508c208ad4f
MR <mlrobins@uw.edu>
2018-03-05 17:12:25 -0800

    graph search works

--- src/final/MapGraph.py
+++ src/final/MapGraph.py
@@ -12,7 +12,7 @@ CONSTANTS
 """
 CAR_WIDTH = 0.3 #m
 CAR_LENGTH = 0.5 #m
-PIXELS_TO_METERS = 0.2
+PIXELS_TO_METERS = 0.02
 MAP_CENTER_OFFSET = None
 
 
@@ -34,9 +34,9 @@ HYPERPARAMETERS
 """
 FILTER_RADIUS = 2 #px
 FILTER_DENS = 5 #px
-BUFFER_RADIUS_M = (0.75 * CAR_LENGTH) #m
+BUFFER_RADIUS_M = (0.6 * CAR_LENGTH) #m
 BUFFER_RADIUS_PX = px(BUFFER_RADIUS_M) #px
-INITIAL_NODE_COVERAGE = 0.0002
+INITIAL_NODE_COVERAGE = 0.0005
 RED_RADIUS_M = 0.2 + BUFFER_RADIUS_M #m
 RED_RADIUS_PX = px(RED_RADIUS_M) #px
 
@@ -76,24 +76,22 @@ class MapGraph:
 
 	def process_map(self):
 		ignore = []
-		"""
-		for i in range(self.h):
-			for j in range(self.w):
-				ct = 0
-				for i2 in range(-FILTER_RADIUS, FILTER_RADIUS+1):
-					for j2 in range(-FILTER_RADIUS, FILTER_RADIUS+1):
-						if not self.inbounds(i+i2, j+j2):
-							continue
-						if self.original_map_mat[i+i2, j+j2] == 255: #?
-							ct += 1
-				if ct < FILTER_DENS:
-					ignore.append((i, j))
-		print "outliers found: " + str(len(ignore))
-		"""
+		# for i in range(self.h):
+		# 	for j in range(self.w):
+		# 		ct = 0
+		# 		for i2 in range(-FILTER_RADIUS, FILTER_RADIUS+1):
+		# 			for j2 in range(-FILTER_RADIUS, FILTER_RADIUS+1):
+		# 				if not self.inbounds(i+i2, j+j2):
+		# 					continue
+		# 				if self.original_map_mat[i+i2, j+j2] == 255: #?
+		# 					ct += 1
+		# 		if ct < FILTER_DENS:
+		# 			ignore.append((i, j))
+		# print "outliers found: " + str(len(ignore))
 
 		for i in range(self.h):
 			for j in range(self.w):
-				if (i % 300 == 0) and (j == 0):
+				if (i % 400 == 0) and (j == 0):
 					print "  " + str(i * 100.0 / self.h) + "%"
 				if (i, j) in ignore:
 					continue
@@ -110,9 +108,9 @@ class MapGraph:
 
 		for i in range(self.h):
 			for j in range(self.w):
-				if (i % 300 == 0) and (j == 0):
+				if (i % 200 == 0) and (j == 0):
 					print "  " + str(i * 100.0 / self.h) + "%"
-				self.processed_map_img.putpixel((i,j), self.processed_map_mat[i,j] * 100)
+				self.processed_map_img.putpixel((i,j), self.processed_map_mat[i,j]*50 + 50 - self.original_map_mat[i,j])
 		# self.processed_map_img.show()
 
 		print "processed map image done"
@@ -130,39 +128,138 @@ class MapGraph:
 	def init_nodes(self, dens=INITIAL_NODE_COVERAGE):
 		allowed = np.nonzero(self.processed_map_mat)
 		num = int(math.floor(dens*len(allowed[0])))
-		selected = np.random.choice(len(allowed[0]), num, replace=False)
+		#selected = np.random.choice(len(allowed[0]), num, replace=False)
 		# print len(allowed[0])
+		stride = int(math.floor(1.0 / dens))
 		print "making " + str(num) + " nodes"
-		for s in selected:
-			# print s
-			# print allowed
-			p = (allowed[0][s], allowed[1][s])
+		# for s in selected:
+		# 	p = (allowed[0][s], allowed[1][s])
+		# 	n = Node(p)
+		# 	self.nodes[n.id] = n
+		for i in range(num):
+			p = (allowed[0][i*stride], allowed[1][i*stride])
 			n = Node(p)
-			self.nodes[n.id] = n
+			self.add(n)
 		print "nodes initialized"
 
-	def connectify(self):
-		for id1, n1 in self.nodes:
-			for id2, n2 in self.nodes:
-				if id1 <= id2:
-					continue
-				if self.connected(n1.pos, n2.pos):
-					n1.add_edge(n2)
-					n2.add_edge(n1)
-					print "a"
+	def connectify(self, nodes=None):
+		if nodes == None:
+			for id1, n1 in self.nodes.iteritems():
+				for id2, n2 in self.nodes.iteritems():
+					if id1 <= id2:
+						continue
+					if self.connected(n1.pos, n2.pos):
+						n1.add_edge(n2)
+						n2.add_edge(n1)
+		else:
+			for n1 in nodes:
+				id1 = n1.id
+				for id2, n2 in self.nodes.iteritems():
+					if id1 <= id2:
+						continue
+					if self.connected(n1.pos, n2.pos):
+						n1.add_edge(n2)
+						n2.add_edge(n1)
+						
 		print "connectified"
 
-	def draw(self):
-		for id1, n1 in self.nodes:
+	# Only call this once
+	def draw(self, show=True):
+		for id1, n1 in self.nodes.iteritems():
 			for id2 in n1.edges:
 				if id1 <= id2:
 					continue
 				n2 = self.nodes[id2]
+				self.processed_map_img.putpixel(n1.pos, 255)
+				self.processed_map_img.putpixel(n2.pos, 255)
+
 				px = Utils.pixels_between(n1.pos, n2.pos)
 				for p in px:
-					 self.processed_map_img.putpixel((i,j), 255)
+					if self.processed_map_img.getpixel(p) != 255:
+						self.processed_map_img.putpixel(p, 175)
+
+		if show:
+			self.processed_map_img.show()
+
+	# Only call this once
+	def draw_path(self, start, finish):
+		self.draw(False)
+
+		self.processed_map_img = self.processed_map_img.convert('RGB')
+
+		path = self.search(start, finish)
+		print path
+		for i in range(len(path)-1):
+			n1 = self.nodes[path[i]]
+			n2 = self.nodes[path[i+1]]
+
+			self.processed_map_img.putpixel(n1.pos, (255,0,0))
+			self.processed_map_img.putpixel(n2.pos, (255,0,0))
+
+			px = Utils.pixels_between(n1.pos, n2.pos)
+			for p in px:
+				self.processed_map_img.putpixel(p, (255,0,0))
 
 		self.processed_map_img.show()
+
+	def add(self, node):
+		if node.name != None:
+			self.named_nodes[node.name] = node
+		self.nodes[node.id] = node
+
+	def rem(self, node):
+		if node.name != None:
+			self.named_nodes.pop(node.name)
+
+		self.nodes.pop(node.id)
+
+		for nid in node.edges:
+			self.nodes[oth].remove_edge(node)
+
+	def search(self, start, finish):
+		# Dijkstra, our old friend
+		dist = {}
+		dist[start.id] = 0
+		prev = {}
+		prev[start.id] = None
+		unvisited = Utils.PriorityQueue()
+		visited = {}
+		visited[start.id] = True
+
+		for nid, n in self.nodes.iteritems():
+			if nid != start.id:
+				dist[nid] = 99999999
+				prev[nid] = None
+				visited[nid] = False
+
+			#unvisited.push(nid, dist[nid])
+		unvisited.push(start.id, 0)
+
+		while not unvisited.isEmpty():
+			curr = unvisited.pop()
+			visited[curr] = True
+			# print curr
+			if curr == finish.id:
+				break
+			else:
+				for id2 in self.nodes[curr].edges:
+					# print id2
+					if visited[id2] == True:
+						continue
+					alt = dist[curr] + self.nodes[curr].edges[id2]
+					if alt < dist[id2]:
+						dist[id2] = alt
+						prev[id2] = curr
+						unvisited.push(id2, alt)
+
+		ret = []
+		c = finish.id
+		while prev[c] != None:
+			ret.append(c)
+			c = prev[c]
+		return ret
+
+
 
 """        
 	def add_blue_and_red(self, filename):
@@ -178,11 +275,6 @@ class MapGraph:
 
 	def read_from_file(self, filename):
 		read map and graph from file
-
-	def draw_graph(self):
-		draw each edge pixel in red
-		draw not allowed areas in grey
-		draw obstacles in black
 
 """
 
@@ -207,4 +299,27 @@ g = MapGraph()
 g.process_map()
 g.init_nodes()
 g.connectify()
-g.draw()
+#g.draw()
+
+b0 = Node((775,775), "B0")
+b1 = Node((1000,1000), "B1")
+g.add(b0)
+g.add(b1)
+g.connectify([b0, b1])
+
+# path = g.search(b0, b1)
+# for n in path:
+# 	print g.nodes[n].pos
+
+# path = g.search(g.nodes[0], g.nodes[230])
+# print path
+# for n in path:
+#  	print g.nodes[n].pos
+
+# g.draw()
+
+print g.search(g.nodes[0], g.nodes[100])
+print g.search(g.nodes[2], g.nodes[150])
+print g.search(g.nodes[6], g.nodes[129])
+
+g.draw_path(g.nodes[2], g.nodes[150])
--- src/final/Utils.py
+++ src/final/Utils.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import heapq
 
 def pixels_between(p1, p2):
     # Bresenham's Line Algorithm
@@ -49,3 +50,52 @@ def pixels_between(p1, p2):
     if swapped:
         points.reverse()
     return points
+
+
+# thanks cse 473
+class PriorityQueue:
+    """
+      Implements a priority queue data structure. Each inserted item
+      has a priority associated with it and the client is usually interested
+      in quick retrieval of the lowest-priority item in the queue. This
+      data structure allows O(1) access to the lowest-priority item.
+
+      Note that this PriorityQueue does not allow you to change the priority
+      of an item.  However, you may insert the same item multiple times with
+      different priorities.
+    """
+    def  __init__(self):
+        self.heap = []
+        self.count = 0
+
+    def push(self, item, priority):
+        # FIXME: restored old behaviour to check against old results better
+        # FIXED: restored to stable behaviour
+        entry = (priority, self.count, item)
+        # entry = (priority, item)
+        heapq.heappush(self.heap, entry)
+        self.count += 1
+
+    def pop(self):
+        (_, _, item) = heapq.heappop(self.heap)
+        #  (_, item) = heapq.heappop(self.heap)
+        return item
+
+    def isEmpty(self):
+        return len(self.heap) == 0
+
+class PriorityQueueWithFunction(PriorityQueue):
+    """
+    Implements a priority queue with the same push/pop signature of the
+    Queue and the Stack classes. This is designed for drop-in replacement for
+    those two classes. The caller has to provide a priority function, which
+    extracts each item's priority.
+    """
+    def  __init__(self, priorityFunction):
+        "priorityFunction (item) -> priority"
+        self.priorityFunction = priorityFunction      # store the priority function
+        PriorityQueue.__init__(self)        # super-class initializer
+
+    def push(self, item):
+        "Adds an item to the queue with priority from the priority function"
+        PriorityQueue.push(self, item, self.priorityFunction(item))
